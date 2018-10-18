90bee5aadfa279a14ea0d5a3be047cad56ba2319
Mueller <philipp.mueller01@sap.com>
2018-10-18 14:38:01 +0200

    Add JS

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Erst loslaufen, wenn das Document Object Modul bereit ist
+window.addEventListener("load", () => {
+    // Anwendung starten
+    let app = new App();
+    app.start();
+});
